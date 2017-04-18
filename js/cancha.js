94981f6dc79cedb92b68d1c8ed5516220b3e73bc
IlseMagali <ilsemke@gmail.com>
2017-04-18 18:30:49 +0000

    las canchas v1

---
+++ js/cancha.js
@@ -0,0 +1,31 @@
+// alert("hola");
+var contenedor = document.getElementById("contenedor");
+var canchaFondo = document.createElement("div");
+var canchaUno = document.createElement("div");
+var porteriaDer = document.createElement("div");
+var porteriaIzq = document.createElement("div");
+var circuloCentral = document.createElement("div");
+
+canchaFondo.id = "canchaFondo";
+canchaUno.id = "canchaUno";
+porteriaDer.id = "poteriaDer";
+porteriaIzq.id = "porteriaIzq";
+circuloCentral.id = "circuloCentral";
+
+// canchaFondo.style.backgroundColor = "green";
+// canchaFondo.style.width = "70" + "%";
+// canchaFondo.style.height = "350" + "px";
+
+canchaUno.style.backgroundColor = "white";
+
+
+console.log(canchaFondo);
+console.log(canchaUno);
+contenedor.appendChild(canchaFondo);
+canchaFondo.appendChild(canchaUno);
+canchaFondo.appendChild(porteriaIzq);
+canchaFondo.appendChild(porteriaDer);
+canchaFondo.appendChild(circuloCentral);
+
+canchaFondo.innerHTML = contenedor;
+// canchaFondo.innerHTML = canchaUno;
